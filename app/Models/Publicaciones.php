d76fdedc5eaf053580b4b3bda793694ff25c7673
Andres Felipe <pizoluligoa@gmail.com>
2023-01-01 15:47:09 -0500

    Se sube proyecto de prueba API REST Laravel Android Studio (Java) EJERCICIO WEB

---
+++ app/Models/Publicaciones.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Publicaciones extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion'
+    ];
+
+    protected $casts = [
+        'property_type' => 'array',
+    ];
+
+}
